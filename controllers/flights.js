be5883a37749f5c8b26afc716b87b96b4a9398ce
mikealberto <alberto.michael1701@gmail.com>
2022-10-05 14:16:28 -0400

    create routes for index page and new page. Create controllers for those routes to render index and new view pages

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,1 +1,9 @@
-module.exports = {};
+module.exports = { get, new: newFlight };
+
+function get(req, res) {
+  res.render("flights/index");
+}
+
+function newFlight(req, res) {
+  res.render("flights/new");
+}
--- routes/flights.js
+++ routes/flights.js
@@ -4,4 +4,7 @@ const flightsCtrl = require("../controllers/flights");
 
 /* GET users listing. */
 
+router.get("/", flightsCtrl.get);
+router.get("/new", flightsCtrl.new);
+
 module.exports = router;
