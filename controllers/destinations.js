f1e7266eec45228b1cda6ce9e57b84b573bdac1c
mikealberto <alberto.michael1701@gmail.com>
2022-10-07 18:08:29 -0400

    complete create function for the destinations controller

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -1,5 +1,15 @@
+const Flight = require("../models/flight");
 module.exports = {create};
 
 function create(req, res) {
-console.log("hello");
+    Flight.findById(req.params.id, function(err, flight) {
+        console.log(req.body);
+        //push subdoc into mongoose array
+        flight.destinations.push(req.body);
+        //save any changes mades to the flight doc
+        flight.save(function(err) {
+            //Responding to the req by redirecting if data has been changed
+            res.redirect(`/flights/${flight._id}`)
+        });
+    });
 }
