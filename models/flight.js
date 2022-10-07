7278865df7c5ab15a65d8c8d5342c14b95c6a0ff
mikealberto <alberto.michael1701@gmail.com>
2022-10-07 14:24:50 -0400

    define destination schema

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+const destinationSchema = new Schema({
+  airport: {
+    type: String, 
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+  },
+  arrival: Date,
+}, {
+  timestamp: true,
+});
 
 const flightSchema = new Schema(
   {
