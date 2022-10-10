508a968b82fbe6ded2fcf6e2dea1f86082be71b3
mikealberto <alberto.michael1701@gmail.com>
2022-10-10 13:33:46 -0400

    replace the show controller function of flights so that it can also get all the tickets that share the same flight id

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,4 +1,5 @@
 const Flight = require("../models/flight");
+const Ticket = require("../models/ticket");
 
 module.exports = { index, new: newFlight, create, show };
 
@@ -30,9 +31,16 @@ function create(req, res) {
   });
 }
 
+// function show(req, res) {
+//   Flight.findById(req.params.id, function(err, flight) {
+//     res.render("flights/show", {flight, title: "Flight Detail"})
+//   });
+// }
 function show(req, res) {
   Flight.findById(req.params.id, function(err, flight) {
-    res.render("flights/show", {flight, title: "Flight Detail"})
+    Ticket.find({flight: flight._id}, function(err, tickets) {
+      console.log(tickets); //DELETE
+      res.render('flights/show', {title:"Flight Detail", flight, tickets});
+    });
   });
-
 }
