2eb84dcb2d9b597cf43b684496dc784a5f8a5c0d
mikealberto <alberto.michael1701@gmail.com>
2022-10-10 19:31:43 -0400

    create form for new ticket and establish route that will lead to the create controller function for the data resource

--- controllers/flights.js
+++ controllers/flights.js
@@ -39,7 +39,7 @@ function create(req, res) {
 function show(req, res) {
   Flight.findById(req.params.id, function(err, flight) {
     Ticket.find({flight: flight._id}, function(err, tickets) {
-      console.log(tickets); //DELETE
+      // console.log(tickets); //DELETE
       res.render("flights/show", {title:"Flight Detail", flight, tickets});
     });
   });
--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,8 +1,13 @@
 const Ticket = require("../models/ticket");
 // const Flight = require("../models/flight")
-module.exports = {new: newTicket};
+module.exports = {new: newTicket, create};
 
 function newTicket (req, res) {
-    console.log(req.params.id) //DELETE
-    res.render("tickets/new", {title: "New Ticket"});
+    // console.log(req.params.id) //DELETE
+    res.render("tickets/new", {title: "New Ticket", flightId: req.params.id});
+}
+
+function create(req, res) {
+    console.log(req.body) //DELETE
+    res.redirect(`/flights/${req.params.id}`);
 }
