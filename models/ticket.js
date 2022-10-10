0f7c9ce8b5ea6503d39bfed4dc2d700d6ab59e28
mikealberto <alberto.michael1701@gmail.com>
2022-10-10 08:12:15 -0400

    Adding flight property to ticketSchema

--- models/ticket.js
+++ models/ticket.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const ticketSchema = new Schema({
     seat: {type: String, match: /[A-F][1-9]\d?/},
     price: {type: Number, min: 0},
-    // flight: {ref: 'Flight' },
+    flight: {type:Schema.Types.ObjectId, ref: 'Flight' },
 }, {
     timestamps: true,
 });
