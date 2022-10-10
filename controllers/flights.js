19f404a35ec33695a260777627e93d80177fd13f
mikealberto <alberto.michael1701@gmail.com>
2022-10-10 14:12:55 -0400

    AAU, when viewing the detail page for a flight, I want to view a list of the tickets that have been created for that flight

--- controllers/flights.js
+++ controllers/flights.js
@@ -40,7 +40,7 @@ function show(req, res) {
   Flight.findById(req.params.id, function(err, flight) {
     Ticket.find({flight: flight._id}, function(err, tickets) {
       console.log(tickets); //DELETE
-      res.render('flights/show', {title:"Flight Detail", flight, tickets});
+      res.render("flights/show", {title:"Flight Detail", flight, tickets});
     });
   });
 }
